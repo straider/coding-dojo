60002ec8a8766c99dbf2f5a90e525e3817deb893
José Carlos Monteiro <jose.monteiro@dowedo-it.com>
2015-05-07 10:55:37 +0100

    Added C# OneLiner to HelloWorld dojo.

---
+++ HelloWorld/OneLiner.cs
@@ -0,0 +1,14 @@
+// Compile command: mcs OneLiner.cs
+// Execute command: mono OneLiner
+
+using System;
+
+public class OneLiner
+{
+
+  public static void Main()
+  {
+    Console.WriteLine( "Hello, World!" );
+  }
+
+}
