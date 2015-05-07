5b197f53f88b410235c58334e20cc93b92f9aba3
José Carlos Monteiro <jose.monteiro@dowedo-it.com>
2015-05-07 01:24:22 +0100

    Added Python ObjectOriented to HelloWorld dojo.

---
+++ HelloWorld/object_oriented.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+class Greeter :
+
+  def __init__( self, name = 'World' ) :
+    self.name = name
+
+  def say( self ) :
+    return self.name
+
+if len( sys.argv ) - 1 == 0 :
+  greeter = Greeter()
+else :
+  greeter = Greeter( sys.argv[ 1 ] )
+print( "Hello, " + greeter.say() + "!" )
+
+print( "» José Carlos Monteiro «" )
