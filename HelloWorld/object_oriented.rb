f5374c19c3710db09573abf96f82d0da44beb27e
José Carlos Monteiro <jose.monteiro@dowedo-it.com>
2015-05-07 01:38:42 +0100

    Added Ruby ObjectOriented to HelloWorld dojo.

---
+++ HelloWorld/object_oriented.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+class Greeter
+
+  def initialize( name = 'World' )
+    @name = name
+  end
+
+  def say()
+    @name
+  end
+
+end
+
+if ARGV.size == 0 then
+  greeter = Greeter.new
+else
+  greeter = Greeter.new( ARGV[ 0 ] )
+end
+puts "Hello, " + greeter.say() + "!"
+
+puts "» José Carlos Monteiro «"
