ca6fd1d15f7dc802612a8d5517027dc75b50d43d
José Carlos Monteiro <jose.monteiro@dowedo-it.com>
2015-05-12 19:11:42 +0100

    Fixed Ruby Object Oriented v3 to FizzBuzz dojo.

--- FizzBuzz/dazz_decorator.rb
+++
@@ -1,9 +0,0 @@
-// Compile command: javac DazzDecorator.java
-
-public class DazzDecorator extends Decorator {
-
-  public DazzDecorator( final Decoder decoder ) {
-    super( 7, "Dazz", decoder );
-  }
-
-}
