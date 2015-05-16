8ef9c5777cd41572b95a436e3e22a32c6ec3d8d4
José Carlos Monteiro <jose.monteiro@dowedo-it.com>
2015-05-16 12:58:25 +0100

    Reviewed code of Ruby FizzBuzz dojo.

---
+++ FizzBuzz/procedural_4.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+1.upto( 100 ) do | i |
+  result = ''
+  result << 'Fizz' if ( i % 3 ) == 0
+  result << 'Buzz' if ( i % 5 ) == 0
+  puts ( result == '' ? '%04d' % i : result )
+end
