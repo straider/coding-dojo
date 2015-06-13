c613e5d40372dfb87a6aa10741b4a3e7c86cbb75
José Carlos Monteiro <jose.monteiro@dowedo-it.com>
2015-06-13 15:59:29 +0100

    Added Ant milestones.

---
+++ Builders/Ant/milestones/ms5-package_run/sources/builder/dojo/Main.java
@@ -0,0 +1,20 @@
+package builder.dojo;
+
+import org.apache.log4j.Logger;
+
+public class Main {
+
+  private static final String messageUTF8 = "» José Carlos Monteiro «";
+  private static final Logger logger      = Logger.getLogger( Main.class );
+
+  public static void main( final String[] arguments ) {
+    final Greeter greeter = ( arguments.length == 0 ) ? new Greeter() : new Greeter( arguments[ 0 ] );
+    final String  message = greeter.greet()
+    logger.info( "Message: " + message );
+    System.out.println( message );
+
+    logger.info( "UTF-8: " + messageUTF8 );
+    System.out.println( messageUTF8 );
+  }
+
+}
