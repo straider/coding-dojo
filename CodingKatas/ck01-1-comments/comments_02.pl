9890339a3543345196b56fcf262dd49e68721cc4
José Carlos Monteiro <straider@sapo.pt>
2016-02-03 23:09:25 +0000

    Added CodingKata 01 solutions - comments and shebang - to Perl dojo.

---
+++ CodingKatas/ck01-1-comments/comments_02.pl
@@ -0,0 +1,5 @@
+=begin
+  A comment
+  that spans across
+  multiple lines.
+=cut
---
+++ CodingKatas/ck01-2-shebang/shebang.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/env perl
+
+use English;
+
+print "Perl Version = $]\n";
+print "Perl Version = $OLD_PERL_VERSION\n";
+
+print "Perl Version = $^V\n";
+print "Perl Version = $PERL_VERSION\n";
+
+print "OS Name = $OSNAME\n";
+print "Process ID \#$PID\n";
