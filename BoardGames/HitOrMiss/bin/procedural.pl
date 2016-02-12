bbe6e03121a1283bc2db862406ae69d5cddbd161
José Carlos Monteiro <straider@sapo.pt>
2016-02-12 11:34:05 +0000

    Improved HitOrMiss challenge solution ( Getopt::Std ) in BoardGames sub-dojo of Perl dojo.

--- BoardGames/HitOrMiss/bin/procedural.pl
+++ BoardGames/HitOrMiss/bin/procedural.pl
@@ -8,11 +8,37 @@ use File::Spec::Functions qw( catdir );
 use FindBin;
 use lib catdir( dirname( $FindBin::Bin ), 'include/libperl' );
 
+use Getopt::Std;
+
 use GameController v1.00.00;
+
+$Getopt::Std::STANDARD_HELP_VERSION = 1;
+
+sub HELP_MESSAGE ( $ ) {
+  my $file_handle = shift;
+
+  print $file_handle "$0 -r|--rows NUMBER_OF_ROWS -c|--columns NUMBER_OF_COLUMNS\n\n";
+  print $file_handle "\t -r|--rows NUMBER_OF_ROWS: set the number of rows ( default is 3 )\n";
+  print $file_handle "\t -c|--columns NUMBER_OF_COLUMNS: set the number of columns ( default is 3 )\n";
+}
+
+sub VERSION_MESSAGE ( $ ) {
+  my $file_handle = shift;
+
+  print $file_handle "HitOrMiss - Procedural 1.00.00\n";
+}
 
 __PACKAGE__ -> main() unless caller;
 
 sub main {
-  # 2016.02.01: Should parse command line options for number of rows and columns ( -r --rows, -c --columns ).
-  play_game( 3, 3 );
+  my $number_of_rows    = 3;
+  my $number_of_columns = 3;
+
+  my %options = ();
+  getopts( 'r:c:', \%options );
+  $number_of_rows    = $options{ r } if ( defined( $options{ r } ) );
+  $number_of_columns = $options{ c } if ( defined( $options{ c } ) );
+
+  print "Playing with a board ${number_of_rows}x${number_of_columns}\n";
+  play_game( $number_of_rows, $number_of_columns );
 }
