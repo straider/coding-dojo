85c9487df17c5db93402ed57e59635ebbe4655e6
José Carlos Monteiro <straider@sapo.pt>
2016-02-12 12:15:14 +0000

    Improved HitOrMiss challenge solution ( Getopt::Long ) in BoardGames sub-dojo of Perl dojo.

--- BoardGames/HitOrMiss/bin/procedural.pl
+++ BoardGames/HitOrMiss/bin/procedural.pl
@@ -8,27 +8,23 @@ use File::Spec::Functions qw( catdir );
 use FindBin;
 use lib catdir( dirname( $FindBin::Bin ), 'include/libperl' );
 
-use Getopt::Std;
+use Getopt::Long;
 
 use GameController v1.00.00;
 
 our $VERSION = 'v1.00.00';
 
-$Getopt::Std::STANDARD_HELP_VERSION = 1;
+sub HELP_MESSAGE () {
+  print "$0 -r|--rows NUMBER_OF_ROWS -c|--columns NUMBER_OF_COLUMNS\n\n";
+  print "\t -r|--rows NUMBER_OF_ROWS: set the number of rows ( default is 3 )\n";
+  print "\t -c|--columns NUMBER_OF_COLUMNS: set the number of columns ( default is 3 )\n";
+  exit 0;
+}
 
-# sub HELP_MESSAGE ( $ ) {
-  # my $file_handle = shift;
-
-  # print $file_handle "$0 -r|--rows NUMBER_OF_ROWS -c|--columns NUMBER_OF_COLUMNS\n\n";
-  # print $file_handle "\t -r|--rows NUMBER_OF_ROWS: set the number of rows ( default is 3 )\n";
-  # print $file_handle "\t -c|--columns NUMBER_OF_COLUMNS: set the number of columns ( default is 3 )\n";
-# }
-
-# sub VERSION_MESSAGE ( $ ) {
-  # my $file_handle = shift;
-
-  # print $file_handle "HitOrMiss - Procedural $VERSION\n";
-# }
+sub VERSION_MESSAGE () {
+  print "HitOrMiss - Procedural $VERSION\n";
+  exit 0;
+}
 
 __PACKAGE__ -> main() unless caller;
 
@@ -37,7 +33,11 @@ sub main {
   my $number_of_columns = 3;
 
   my %options = ();
-  getopts( 'r:c:', \%options );
+  GetOptions( 'rows=i'    => \$options{ r }
+            , 'columns=i' => \$options{ c }
+            , 'help'      => sub { HELP_MESSAGE    () }
+            , 'version'   => sub { VERSION_MESSAGE () }
+            ) or die( "Unable to parse command line options!\n" );
   $number_of_rows    = $options{ r } if ( defined( $options{ r } ) );
   $number_of_columns = $options{ c } if ( defined( $options{ c } ) );
 
