fc395246cbb0d99aa05ef2bdf5a345871cc1e36d
José Carlos Monteiro <straider@sapo.pt>
2016-02-14 13:59:39 +0000

    Improved HitOrMiss challenge procedural skeleton ( optparse ) of BoardGames sub-dojo in Python dojo.

--- BoardGames/HitOrMiss/bin/procedural.py
+++ BoardGames/HitOrMiss/bin/procedural.py
@@ -7,39 +7,23 @@ import sys
 top_folder = os.path.dirname( sys.path[ 0 ] )
 sys.path.append( os.path.join( top_folder, 'include', 'libpython' ) )
 
-import getopt
+import optparse
 
 import game_controller
 
 VERSION = 'v1.00.00'
 PROGRAM = sys.argv[ 0 ]
 
-def show_usage( exit_code = 0 ) :
-  print '{0} -r|--rows NUMBER_OF_ROWS -c|--columns NUMBER_OF_COLUMNS\n'.format( PROGRAM )
-  print '\t -r|--rows NUMBER_OF_ROWS: set the number of rows ( default is 3 )'
-  print '\t -c|--columns NUMBER_OF_COLUMNS: set the number of columns ( default is 3 )'
-  sys.exit( exit_code )
-
-def show_version() :
-  print 'HitOrMiss - Procedural {0}'.format( VERSION )
-  sys.exit( 0 )
-
 if __name__ == '__main__' :
-  number_of_rows    = 3
-  number_of_columns = 3
+  parser = optparse.OptionParser( usage   = '%prog [ -r|--rows ROWS ] [ -c|--columns COLUMNS ]'
+                                , version = 'HitOrMiss - Procedural {0}'.format( VERSION )
+                                )
+  parser.add_option( '-r', '--rows'   , action = 'store', dest = 'rows'   , type = 'int', help = 'set number of rows ( default is 3 )'    )
+  parser.add_option( '-c', '--columns', action = 'store', dest = 'columns', type = 'int', help = 'set number of columns ( default is 3 )' )
 
-  try:
-    opts, args = getopt.getopt( sys.argv[ 1 : ], 'hvr:c:', [ 'help', 'version', 'rows=', 'columns=' ] )
-  except getopt.GetoptError :
-    show_usage( -1 )
-  for opt, arg in opts :
-    if   opt in ( '-h', '--help'    ) :
-      show_usage()
-    elif opt in ( '-v', '--version' ) :
-      show_version()
-    elif opt in ( '-r', '--rows'    ) :
-      number_of_rows = arg
-    elif opt in ( '-c', '--columns' ) :
-      number_of_columns = arg
+  options, remainder = parser.parse_args()
+
+  number_of_rows    = options.rows    if options.rows    else 3
+  number_of_columns = options.columns if options.columns else 3
 
   game_controller.play_game( number_of_rows, number_of_columns )
