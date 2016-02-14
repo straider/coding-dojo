b2dcde79ac8c142748ff5605fd9f34251f1632ad
José Carlos Monteiro <straider@sapo.pt>
2016-02-14 14:15:17 +0000

    Improved HitOrMiss challenge procedural skeleton ( argparse ) of BoardGames sub-dojo in Python dojo.

--- BoardGames/HitOrMiss/bin/procedural.py
+++ BoardGames/HitOrMiss/bin/procedural.py
@@ -7,7 +7,7 @@ import sys
 top_folder = os.path.dirname( sys.path[ 0 ] )
 sys.path.append( os.path.join( top_folder, 'include', 'libpython' ) )
 
-import optparse
+import argparse
 
 import game_controller
 
@@ -18,17 +18,17 @@ def show_version() :
   sys.exit( 0 )
 
 if __name__ == '__main__' :
-  parser = optparse.OptionParser( usage   = '%prog [ -r|--rows ROWS ] [ -c|--columns COLUMNS ] [ -v|--version ]' )
-  parser.add_option( '-r', '--rows'   , action = 'store'     , dest = 'rows'   , type = 'int', help = 'set number of rows ( default is 3 )'    )
-  parser.add_option( '-c', '--columns', action = 'store'     , dest = 'columns', type = 'int', help = 'set number of columns ( default is 3 )' )
-  parser.add_option( '-v', '--version', action = 'store_true', dest = 'version'              , help = 'show program information and exit'      )
+  parser = argparse.ArgumentParser( usage = '%(prog)s [ -r|--rows ROWS ] [ -c|--columns COLUMNS ] [ -v|--version ]' )
+  parser.add_argument( '-r', '--rows'   , action = 'store'     , dest = 'rows'   , type = int, help = 'set number of rows ( default is 3 )'    )
+  parser.add_argument( '-c', '--columns', action = 'store'     , dest = 'columns', type = int, help = 'set number of columns ( default is 3 )' )
+  parser.add_argument( '-v', '--version', action = 'store_true', dest = 'version'            , help = 'show program information and exit'      )
 
-  options, remainder = parser.parse_args()
+  arguments = parser.parse_args()
 
-  if options.version :
+  if arguments.version :
     show_version()
 
-  number_of_rows    = options.rows    if options.rows    else 3
-  number_of_columns = options.columns if options.columns else 3
+  number_of_rows    = arguments.rows    if arguments.rows    else 3
+  number_of_columns = arguments.columns if arguments.columns else 3
 
   game_controller.play_game( number_of_rows, number_of_columns )
