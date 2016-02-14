22bcf3768703cb75e23e39714bc36359182695aa
José Carlos Monteiro <straider@sapo.pt>
2016-02-14 14:03:14 +0000

    Improved HitOrMiss challenge procedural skeleton ( optparse version ) of BoardGames sub-dojo in Python dojo.

--- BoardGames/HitOrMiss/bin/procedural.py
+++ BoardGames/HitOrMiss/bin/procedural.py
@@ -12,16 +12,21 @@ import optparse
 import game_controller
 
 VERSION = 'v1.00.00'
-PROGRAM = sys.argv[ 0 ]
+
+def show_version() :
+  print 'HitOrMiss - Procedural {0}'.format( VERSION )
+  sys.exit( 0 )
 
 if __name__ == '__main__' :
-  parser = optparse.OptionParser( usage   = '%prog [ -r|--rows ROWS ] [ -c|--columns COLUMNS ]'
-                                , version = 'HitOrMiss - Procedural {0}'.format( VERSION )
-                                )
-  parser.add_option( '-r', '--rows'   , action = 'store', dest = 'rows'   , type = 'int', help = 'set number of rows ( default is 3 )'    )
-  parser.add_option( '-c', '--columns', action = 'store', dest = 'columns', type = 'int', help = 'set number of columns ( default is 3 )' )
+  parser = optparse.OptionParser( usage   = '%prog [ -r|--rows ROWS ] [ -c|--columns COLUMNS ] [ -v|--version ]' )
+  parser.add_option( '-r', '--rows'   , action = 'store'     , dest = 'rows'   , type = 'int', help = 'set number of rows ( default is 3 )'    )
+  parser.add_option( '-c', '--columns', action = 'store'     , dest = 'columns', type = 'int', help = 'set number of columns ( default is 3 )' )
+  parser.add_option( '-v', '--version', action = 'store_true', dest = 'version'              , help = 'show program information and exit'      )
 
   options, remainder = parser.parse_args()
+
+  if options.version :
+    show_version()
 
   number_of_rows    = options.rows    if options.rows    else 3
   number_of_columns = options.columns if options.columns else 3
