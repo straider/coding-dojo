9079c908dbb9d561e79a1afac2ad1d00f97d58d5
José Carlos Monteiro <straider@sapo.pt>
2016-03-31 00:02:33 +0100

    Improved HitOrMiss challenge procedural skeleton ( ARGV ) of BoardGames sub-dojo in Ruby dojo.

--- BoardGames/HitOrMiss/bin/procedural.rb
+++ BoardGames/HitOrMiss/bin/procedural.rb
@@ -27,19 +27,15 @@ if __FILE__ == $0
   loop do
     case ARGV[ 0 ]
         when '-h', '--help'
-          ARGV.shift
           show_help
           exit( 0 )
         when '-v', '--version'
-          ARGV.shift
           show_version
         when '-r', '--rows'
-          ARGV.shift
-          number_of_rows = ARGV[ 0 ]
+          number_of_rows = ARGV[ 1 ].to_i
           ARGV.shift
         when '-c', '--columns'
-          ARGV.shift
-          number_of_columns = ARGV[ 0 ]
+          number_of_columns = ARGV[ 1 ].to_i
           ARGV.shift
         when /^-/
           puts "Unknown option: #{ ARGV[ 0 ].inspect }"
@@ -48,6 +44,7 @@ if __FILE__ == $0
         else
           break
     end
+    ARGV.shift
   end
 
   print "Playing with a board {#{ number_of_rows }}x{#{ number_of_columns }}"
