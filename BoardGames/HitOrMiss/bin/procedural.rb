b48f35b12faa30460f4ad8d40fd2238e2424cc9d
José Carlos Monteiro <straider@sapo.pt>
2016-04-01 20:39:31 +0100

    Improved HitOrMiss challenge procedural skeleton ( OptionParser ) of BoardGames sub-dojo in Ruby dojo.

--- BoardGames/HitOrMiss/bin/procedural.rb
+++ BoardGames/HitOrMiss/bin/procedural.rb
@@ -5,7 +5,7 @@ top_folder       = File.dirname( File.dirname( File.expand_path( __FILE__ ) ) )
 libraries_folder = File.join( top_folder, 'include', 'libruby' )
 $LOAD_PATH.unshift( libraries_folder ) unless $LOAD_PATH.include?( libraries_folder )
 
-require 'getoptlong'
+require 'optparse'
 
 require 'game_controller'
 
@@ -13,37 +13,33 @@ VERSION = 'v1.00.00'
 
 def show_help()
   print "#{ $0 } -r|--rows NUMBER_OF_ROWS -c|--columns NUMBER_OF_COLUMNS\n\n";
-  print "\t -r|--rows NUMBER_OF_ROWS: set the number of rows ( default is 3 )\n";
-  print "\t -c|--columns NUMBER_OF_COLUMNS: set the number of columns ( default is 3 )\n";
+  puts "\t -r|--rows NUMBER_OF_ROWS: set the number of rows ( default is 3 )";
+  puts "\t -c|--columns NUMBER_OF_COLUMNS: set the number of columns ( default is 3 )";
 end
 
 def show_version()
   puts "HitOrMiss - Procedural #{ VERSION }"
-  exit( 0 )
 end
 
 if __FILE__ == $0
 
   number_of_rows    = 3
   number_of_columns = 3
-  options = GetoptLong.new( [ '--help'   , '-h', GetoptLong::NO_ARGUMENT       ] \
-                          , [ '--version', '-v', GetoptLong::NO_ARGUMENT       ] \
-                          , [ '--rows'   , '-r', GetoptLong::REQUIRED_ARGUMENT ] \
-                          , [ '--columns', '-c', GetoptLong::REQUIRED_ARGUMENT ]
-                          )
+  parser = OptionParser.new do | options |
+    options.version = VERSION
 
-  options.each do | option, argument |
-    case option
-      when '--help'
-        show_help
-        exit( 0 )
-      when '--version'
-        show_version
-      when '--rows'
-        number_of_rows = argument.to_i
-      when '--columns'
-        number_of_columns = argument.to_i
-    end
+    options.on( '-h', '--help'                              , 'Show help message'     ) { show_help   ; exit( 0 ) }
+    options.on( '-v', '--version'                           , 'Show program version'  ) { show_version; exit( 0 ) }
+    options.on( '-r', '--rows NUMBER_OF_ROWS'      , Integer, 'Set number of rows'    ) { | argument | number_of_rows    = argument }
+    options.on( '-c', '--columns NUMBER_OF_COLUMNS', Integer, 'Set number of columns' ) { | argument | number_of_columns = argument }
+  end
+
+  begin
+    parser.parse!
+  rescue OptionParser::ParseError => error
+    puts "#{ error }"
+    show_help
+    exit( -1 )
   end
 
   puts "Playing with a board {#{ number_of_rows }}x{#{ number_of_columns }}"
