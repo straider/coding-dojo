8b0061f9f90be35a2f1ba16dc39aa15f614eb1a5
José Carlos Monteiro <straider@sapo.pt>
2016-04-01 20:12:12 +0100

    Improved HitOrMiss challenge procedural skeleton ( GetoptLong ) of BoardGames sub-dojo in Ruby dojo.

--- BoardGames/HitOrMiss/bin/procedural.rb
+++ BoardGames/HitOrMiss/bin/procedural.rb
@@ -5,6 +5,8 @@ top_folder       = File.dirname( File.dirname( File.expand_path( __FILE__ ) ) )
 libraries_folder = File.join( top_folder, 'include', 'libruby' )
 $LOAD_PATH.unshift( libraries_folder ) unless $LOAD_PATH.include?( libraries_folder )
 
+require 'getoptlong'
+
 require 'game_controller'
 
 VERSION = 'v1.00.00'
@@ -24,29 +26,26 @@ if __FILE__ == $0
 
   number_of_rows    = 3
   number_of_columns = 3
-  loop do
-    case ARGV[ 0 ]
-      when '-h', '--help'
+  options = GetoptLong.new( [ '--help'   , '-h', GetoptLong::NO_ARGUMENT       ] \
+                          , [ '--version', '-v', GetoptLong::NO_ARGUMENT       ] \
+                          , [ '--rows'   , '-r', GetoptLong::REQUIRED_ARGUMENT ] \
+                          , [ '--columns', '-c', GetoptLong::REQUIRED_ARGUMENT ]
+                          )
+
+  options.each do | option, argument |
+    case option
+      when '--help'
         show_help
         exit( 0 )
-      when '-v', '--version'
+      when '--version'
         show_version
-      when '-r', '--rows'
-        number_of_rows = ARGV[ 1 ].to_i
-        ARGV.shift
-      when '-c', '--columns'
-        number_of_columns = ARGV[ 1 ].to_i
-        ARGV.shift
-      when /^-/
-        puts "Unknown option: #{ ARGV[ 0 ].inspect }"
-        show_help
-        exit( -1 )
-      else
-        break
+      when '--rows'
+        number_of_rows = argument.to_i
+      when '--columns'
+        number_of_columns = argument.to_i
     end
-    ARGV.shift
   end
 
-  print "Playing with a board {#{ number_of_rows }}x{#{ number_of_columns }}"
+  puts "Playing with a board {#{ number_of_rows }}x{#{ number_of_columns }}"
   play_game( number_of_rows, number_of_columns )
 end
