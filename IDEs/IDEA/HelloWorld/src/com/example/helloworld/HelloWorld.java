6978eff0d06baad772d9f3668628bf01d114b9a0
José Carlos Monteiro <straider@sapo.pt>
2016-05-27 08:44:24 +0100

    Finished "Debugging Your First Java Application".

--- IDEs/IDEA/HelloWorld/src/com/example/helloworld/HelloWorld.java
+++ IDEs/IDEA/HelloWorld/src/com/example/helloworld/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 
     public static void main( final String[] arguments ) {
         System.out.println( "Hello, World!" );
+        System.out.println( "it's me, Wonbat!" );
     }
 
 }
