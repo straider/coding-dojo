1daf4ea7969f418edebaecc454d3e7d5e37fa6cc
José Carlos Monteiro <straider@sapo.pt>
2016-06-07 15:53:50 +0100

    Added String Calculator Kata main program.

---
+++ IDEs/IDEA/StringCalculatorKata/src/java/com/javacodegeeks/examples/Main.java
@@ -0,0 +1,14 @@
+//https://examples.javacodegeeks.com/desktop-java/ide/intellij-idea-tutorial-beginners/
+
+package com.javacodegeeks.examples;
+
+public class Main {
+
+    public static void main( final String[] arguments ) {
+        final StringCalculator calculator = new StringCalculator();
+
+        final String numbers = "//[***][%]\n1***2%3";
+        final Integer result = calculator.add( numbers );
+        System.out.println( "output = " + result );
+    }
+}
