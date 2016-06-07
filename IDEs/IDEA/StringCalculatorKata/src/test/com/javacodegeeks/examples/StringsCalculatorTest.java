32e2f9be7cc046cf1c94895040f452a6767ea47b
José Carlos Monteiro <straider@sapo.pt>
2016-06-07 15:11:51 +0100

    Added String Calculator Kata test cases.

---
+++ IDEs/IDEA/StringCalculatorKata/src/test/com/javacodegeeks/examples/StringsCalculatorTest.java
@@ -0,0 +1,35 @@
+package com.javacodegeeks.examples;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class StringsCalculatorTest {
+
+    private final StringCalculator calculator;
+
+    @Before
+    public void init() {
+        calculator = new StringCalculator();
+    }
+
+    @Test
+    public void testDefaultDelimiters() {
+        final String input = "1\n2,3";
+        assertEquals( 6, calculator.add( input ) );
+    }
+
+    @Test
+    public void testMultiDelimiters() {
+        final String input = "//[***][%]\n1***2%3";
+        assertEquals( 6, calculator.add( input ) );
+    }
+
+    @Test
+    public void testNumbersMoreThan1000() {
+        final String input = "//[***][%]\n1***2%3,2000";
+        assertEquals( 6, calculator.add( input ) );
+    }
+
+}
