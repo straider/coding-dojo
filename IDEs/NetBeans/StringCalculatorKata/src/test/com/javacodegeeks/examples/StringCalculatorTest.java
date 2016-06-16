24fb21ece2d3cf941871704ea716efa36b218365
José Carlos Monteiro <straider@sapo.pt>
2016-06-16 22:33:22 +0100

    Added String Calculator Kata test cases.

--- IDEs/NetBeans/StringCalculatorKata/src/test/com/javacodegeeks/examples/StringCalculatorTest.java
+++ IDEs/NetBeans/StringCalculatorKata/src/test/com/javacodegeeks/examples/StringCalculatorTest.java
@@ -8,6 +8,8 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class StringCalculatorTest {
 
     private StringCalculator calculator;
