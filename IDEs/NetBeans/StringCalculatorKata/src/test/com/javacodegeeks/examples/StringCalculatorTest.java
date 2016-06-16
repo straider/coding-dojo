453d952d12e495cf92ac7d5a18addcf3f430e694
José Carlos Monteiro <straider@sapo.pt>
2016-06-16 22:23:49 +0100

    Added String Calculator Kata test cases.

---
+++ IDEs/NetBeans/StringCalculatorKata/src/test/com/javacodegeeks/examples/StringCalculatorTest.java
@@ -0,0 +1,53 @@
+//https://examples.javacodegeeks.com/desktop-java/ide/intellij-idea-tutorial-beginners/
+
+package com.javacodegeeks.examples;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class StringCalculatorTest {
+
+    private StringCalculator calculator;
+
+    public StringCalculatorTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+        calculator = new StringCalculator();
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testDefaultDelimiters() {
+        final String input = "1\n2,3";
+        assertEquals( 6, calculator.add( input ) );
+    }
+
+    @Test
+    public void testMultiDelimiters() {
+        final String input = "//[***][%]\n1***2%3";
+        assertEquals( 6, calculator.add( input ) );
+    }
+
+    @Test
+    public void testNumbersMoreThan1000() {
+        final String input = "//[***][%]\n1***2%3,2000";
+        assertEquals( 6, calculator.add( input ) );
+    }
+
+}
