74b03b11f8851fe6d073617dd5f4368a11a70f25
José Carlos Monteiro <straider@sapo.pt>
2016-06-16 23:44:21 +0100

    Added String Calculator Kata logic to solve test cases.

---
+++ IDEs/Eclipse/StringCalculatorKata/src/java/com/javacodegeeks/examples/InputNormalizer.java
@@ -0,0 +1,44 @@
+//https://examples.javacodegeeks.com/desktop-java/ide/intellij-idea-tutorial-beginners/
+
+package com.javacodegeeks.examples;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class InputNormalizer {
+
+    List< String > normalize( final String input ) {
+        String result = input;
+
+        String delimiters = "\n|,";
+        if ( input.contains( "//" ) ) {
+            delimiters = getDelimiters( result, delimiters );
+            result     = stripDelimiters( result );
+        }
+
+        return Arrays.asList( result.split( delimiters ) );
+    }
+
+    private String getDelimiters( final String input, final String defaultDelimiters ) {
+        final StringBuilder delimiters = new StringBuilder( defaultDelimiters );
+
+        String numbers = input;
+        while ( numbers.indexOf( "[" ) >= 0 ) {
+            delimiters.append( "|" );
+            final String delimiter = numbers.substring( numbers.indexOf( "[" ) + 1, numbers.indexOf( "]" ) );
+            for ( final char delimiterChar : delimiter.toCharArray() ) {
+                delimiters.append( "\\" ).append( delimiterChar );
+            }
+            numbers = numbers.substring( numbers.indexOf( "]" ) + 1 );
+        }
+
+        System.out.println(" delimiters.toString() = " + delimiters.toString());
+        return delimiters.toString();
+    }
+
+    private String stripDelimiters( final String numbers ) {
+        return numbers.substring( numbers.indexOf( "\n" ) + 1 );
+    }
+
+
+}
