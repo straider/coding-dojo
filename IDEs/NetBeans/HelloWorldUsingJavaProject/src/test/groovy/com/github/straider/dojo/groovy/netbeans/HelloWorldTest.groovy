8c7b36d68557e0c63a4f69e60ab91cecd65a8044
José Carlos Monteiro <straider@sapo.pt>
2016-10-08 01:16:28 +0100

    Cleaned up generated code.

--- IDEs/NetBeans/HelloWorldUsingJavaProject/src/test/groovy/com/github/straider/dojo/groovy/netbeans/HelloWorldTest.groovy
+++ IDEs/NetBeans/HelloWorldUsingJavaProject/src/test/groovy/com/github/straider/dojo/groovy/netbeans/HelloWorldTest.groovy
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.github.straider.dojo.groovy.netbeans
 
 import org.junit.After
@@ -11,12 +5,7 @@ import org.junit.AfterClass
 import org.junit.Before
 import org.junit.BeforeClass
 import org.junit.Test
-import static org.junit.Assert.*
 
-/**
- *
- * @author jocamo
- */
 class HelloWorldTest {
 
     public HelloWorldTest() {
@@ -37,9 +26,5 @@ class HelloWorldTest {
     @After
     public void tearDown() {
     }
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    
 }
